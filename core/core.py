90c42326f1394efd15efca63334d24137969af7a
amacedo <armando_m@live.com>
2016-10-20 23:39:16 -0500

    added new files for the core functionality, class PFHandler to handle public figure requests and core.py to be the main program of the core

---
+++ core/core.py
@@ -0,0 +1,13 @@
+"""
+A python module that provides methods 
+to collect public figure information. 
+"""
+
+import os
+import warnings
+from PFHandler import PFHandler
+
+print "hola"
+pfhandler = PFHandler()
+pfhandler.general_info('cosmosmidnight')
+
---
+++ core/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+""" This script setups the needed environment variables for the different API's 
+ * that are used to scrap all data from the social networks. """
+
+import os
+
+os.environ['FACEBOOK_APP_ID'] = "id"
+
+print os.environ['FACEBOOK_APP_ID']
